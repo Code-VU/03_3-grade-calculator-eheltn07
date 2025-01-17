407a0897968f6933d280a7ecda5b91c9d42870b9
Eric Helton <Eric.Helton@vu.com>
2025-01-17 06:46:14 -0600

    grade calc update 1

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,10 +3,40 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
+bad_score_message = "Bad score"
+
+try:
+    score = float(input("Enter score: "))
+
+    if score > 1.0:
+        grade = bad_score_message
+    elif score >= 0.9 and score <= 1.0:
+        grade = "A"
+    elif score >= 0.8:
+        grade = "B"
+    elif score >= 0.7:
+        grade = "C"
+    elif score >= 0.6:
+        grade = "D"
+    elif score < 0.6:
+        grade = "F"
+    else:
+        grade = bad_score_message
+
+except:
+    grade= bad_score_message
+
+print (grade)
 
     # end assignment
-
+''' 
+Score   Grade
+>= 0.9     A
+>= 0.8     B
+>= 0.7     C
+>= 0.6     D
+ < 0.6     F
+ '''
 ## If you want to test locally before you try to sync
 ## Run > python calculateGrade.py
 
